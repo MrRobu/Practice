c7d15d3f3cec3645639d29c14e61a6e474e36663
MrRobu <37081266+MrRobu@users.noreply.github.com>
2018-06-23 11:17:22 +0300

    Tutorial: Imprelentation of Caliburn Micro

---
+++ Foo/Foo/ViewModels/FirstChildViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foo.ViewModels
+{
+    public class FirstChildViewModel : Screen
+    {
+
+    }
+}
