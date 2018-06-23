5966240eaec1306f941bf0702b5e263735b0ebf6
MrRobu <37081266+MrRobu@users.noreply.github.com>
2018-06-23 16:33:03 +0300

    Connection testing

--- Foo/Foo/ViewModels/ShellViewModel.cs
+++ Foo/Foo/ViewModels/ShellViewModel.cs
@@ -4,10 +4,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Odbc;
+using System.Data;
+using System.Windows;
 
 namespace Foo.ViewModels
 {
     public class ShellViewModel : Conductor<object>
     {
-    }       
+        OdbcConnection connection;
+        public ShellViewModel()
+        {
+            OpenConnection();
+        }
+
+        public void OpenConnection()
+        {
+            try
+            {
+                connection = new OdbcConnection(Helper.ConnectionString("postgres"));
+                connection.Open();
+                
+                MessageBox.Show("Connection Open");
+                connection.Close();
+            }
+            catch (OdbcException error)
+            {
+                MessageBox.Show(error.Message);
+                if (connection?.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
 }
